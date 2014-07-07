dd6b6060ad8ee422abd952d97c150792412d07c0
张宇翔 <zz593141477@gmail.com>
2014-07-07 23:27:26 +0800

    USB SD card reader works

--- project/usb/src/mass_mal.c
+++ project/usb/src/mass_mal.c
@@ -28,6 +28,8 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "mass_mal.h"
+#include "common.h"
+#include "sdio.h"
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
@@ -38,9 +40,7 @@ uint32_t Mass_Block_Size[2];
 uint32_t Mass_Block_Count[2];
 __IO uint32_t Status = 0;
 
-#if defined(USE_STM3210E_EVAL) || defined(USE_STM32L152D_EVAL)
 SD_CardInfo mSDCardInfo;
-#endif
 
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
@@ -58,9 +58,8 @@ uint16_t MAL_Init(uint8_t lun)
   switch (lun)
   {
     case 0:
-#if 0
       Status = SD_Init();
-#endif
+      LOG_DBG("SD_Init()=%d", Status);
       break;
 // #ifdef USE_STM3210E_EVAL
 //     case 1:
@@ -85,17 +84,16 @@ uint16_t MAL_Write(uint8_t lun, uint32_t Memory_Offset, uint32_t *Writebuff, uin
   switch (lun)
   {
     case 0:
-#if 0
-    Status = SD_WriteMultiBlocks((uint8_t*)Writebuff, Memory_Offset, Transfer_Length,1);
-#if defined(USE_STM3210E_EVAL) || defined(USE_STM32L152D_EVAL)
-    Status = SD_WaitWriteOperation();  
+    // Status = SD_WriteMultiBlocks((uint8_t*)Writebuff, Memory_Offset, Transfer_Length,1);
+    SD_WriteBlock((uint8_t*)Writebuff, Memory_Offset, Transfer_Length);
+// #if defined(USE_STM3210E_EVAL) || defined(USE_STM32L152D_EVAL)
+    // Status = SD_WaitWriteOperation();  
     while(SD_GetStatus() != SD_TRANSFER_OK);
       if ( Status != SD_OK )
       {
         return MAL_FAIL;
       }      
-#endif /* USE_STM3210E_EVAL ||USE_STM32L152D_EVAL*/      
-#endif
+// #endif /* USE_STM3210E_EVAL ||USE_STM32L152D_EVAL*/      
       break;
 // #ifdef USE_STM3210E_EVAL
 //     case 1:
@@ -121,10 +119,12 @@ uint16_t MAL_Read(uint8_t lun, uint32_t Memory_Offset, uint32_t *Readbuff, uint1
   switch (lun)
   {
     case 0:
-#if 0
-      SD_ReadMultiBlocks((uint8_t*)Readbuff, Memory_Offset, Transfer_Length, 1);
-#if defined(USE_STM3210E_EVAL) || defined(USE_STM32L152D_EVAL)
-      Status = SD_WaitReadOperation();
+      // SD_ReadMultiBlocks((uint8_t*)Readbuff, Memory_Offset, Transfer_Length, 1);
+      // LOG_DBG("SD_ReadBlock(%d,%d,%d)", Readbuff, Memory_Offset, Transfer_Length);
+      SD_ReadBlock((uint8_t*)Readbuff, Memory_Offset, Transfer_Length);
+
+// #if defined(USE_STM3210E_EVAL) || defined(USE_STM32L152D_EVAL)
+      // Status = SD_WaitReadOperation();
       while(SD_GetStatus() != SD_TRANSFER_OK)
       {
       }
@@ -133,8 +133,7 @@ uint16_t MAL_Read(uint8_t lun, uint32_t Memory_Offset, uint32_t *Readbuff, uint1
       {
         return MAL_FAIL;
       }
-#endif /* USE_STM3210E_EVAL */      
-#endif
+// #endif /* USE_STM3210E_EVAL */      
       break;
 // #ifdef USE_STM3210E_EVAL
 //     case 1:
@@ -157,27 +156,27 @@ uint16_t MAL_Read(uint8_t lun, uint32_t Memory_Offset, uint32_t *Readbuff, uint1
 *******************************************************************************/
 uint16_t MAL_GetStatus (uint8_t lun)
 {
-#if 0
-#ifdef USE_STM3210E_EVAL
-  NAND_IDTypeDef NAND_ID;
+// #ifdef USE_STM3210E_EVAL
+//   NAND_IDTypeDef NAND_ID;
   uint32_t DeviceSizeMul = 0, NumberOfBlocks = 0;
-#else
-  SD_CSD SD_csd;
-  uint32_t DeviceSizeMul = 0;
-#endif /* USE_STM3210E_EVAL */
+// #else
+//   SD_CSD SD_csd;
+//   uint32_t DeviceSizeMul = 0;
+// #endif /* USE_STM3210E_EVAL */
 
-#ifdef USE_STM32L152D_EVAL
+// #ifdef USE_STM32L152D_EVAL
 
-  uint32_t NumberOfBlocks = 0;
-#endif
+//   uint32_t NumberOfBlocks = 0;
+// #endif
 
   if (lun == 0)
   {
-#if defined (USE_STM3210E_EVAL)  || defined(USE_STM32L152D_EVAL)
+// #if defined (USE_STM3210E_EVAL)  || defined(USE_STM32L152D_EVAL)
     if (SD_Init() == SD_OK)
     {
       SD_GetCardInfo(&mSDCardInfo);
-      SD_SelectDeselect((uint32_t) (mSDCardInfo.RCA << 16));
+      LOG_DBG("SD_GetCardInfo()");
+      // SD_SelectDeselect((uint32_t) (mSDCardInfo.RCA << 16));
       DeviceSizeMul = (mSDCardInfo.SD_csd.DeviceSizeMul + 2);
 
       if(mSDCardInfo.CardType == SDIO_HIGH_CAPACITY_SD_CARD)
@@ -191,30 +190,30 @@ uint16_t MAL_GetStatus (uint8_t lun)
       }
       Mass_Block_Size[0]  = 512;
 
-      Status = SD_SelectDeselect((uint32_t) (mSDCardInfo.RCA << 16)); 
-      Status = SD_EnableWideBusOperation(SDIO_BusWide_4b); 
+      // Status = SD_SelectDeselect((uint32_t) (mSDCardInfo.RCA << 16)); 
+      // Status = SD_EnableWideBusOperation(SDIO_BusWide_4b); 
       if ( Status != SD_OK )
       {
         return MAL_FAIL;
       }
      
-#else
+// #else
 
-    uint32_t temp_block_mul = 0;
-    SD_GetCSDRegister(&SD_csd);
-    DeviceSizeMul = SD_csd.DeviceSizeMul + 2;
-    temp_block_mul = (1 << SD_csd.RdBlockLen)/ 512;
-    Mass_Block_Count[0] = ((SD_csd.DeviceSize + 1) * (1 << (DeviceSizeMul))) * temp_block_mul;
-    Mass_Block_Size[0] = 512;
-    Mass_Memory_Size[0] = (Mass_Block_Count[0] * Mass_Block_Size[0]);
-#endif /* USE_STM3210E_EVAL */
+//     uint32_t temp_block_mul = 0;
+//     SD_GetCSDRegister(&SD_csd);
+//     DeviceSizeMul = SD_csd.DeviceSizeMul + 2;
+//     temp_block_mul = (1 << SD_csd.RdBlockLen)/ 512;
+//     Mass_Block_Count[0] = ((SD_csd.DeviceSize + 1) * (1 << (DeviceSizeMul))) * temp_block_mul;
+//     Mass_Block_Size[0] = 512;
+//     Mass_Memory_Size[0] = (Mass_Block_Count[0] * Mass_Block_Size[0]);
+// #endif /* USE_STM3210E_EVAL */
       Mass_Memory_Size[0] = Mass_Block_Count[0] * Mass_Block_Size[0];
-      STM_EVAL_LEDOn(LED2);
+      // STM_EVAL_LEDOn(LED2);
       return MAL_OK;
 
-#if defined (USE_STM3210E_EVAL) || defined(USE_STM32L152D_EVAL)
+// #if defined (USE_STM3210E_EVAL) || defined(USE_STM32L152D_EVAL)
     }
-#endif /* USE_STM3210E_EVAL */
+// #endif /* USE_STM3210E_EVAL */
   }
 // #ifdef USE_STM3210E_EVAL
 //   else
@@ -230,8 +229,7 @@ uint16_t MAL_GetStatus (uint8_t lun)
 //     }
 //   }
 // #endif /* USE_STM3210E_EVAL */
-  STM_EVAL_LEDOn(LED2);
-#endif
+  // STM_EVAL_LEDOn(LED2);
   return MAL_FAIL;
 }
 
